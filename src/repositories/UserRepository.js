e1c41fd86636fe95f0eeca082d409caff091ec7a
Luiz Lopes <luizrl@yahoo.com.br>
2022-06-24 13:37:49 -0300

    criação dos testes para cadastro de usuário e controle de execeção de emails

--- src/repositories/UserRepository.js
+++ src/repositories/UserRepository.js
@@ -9,7 +9,7 @@ class UserRepository {
     return user
   }
 
-  async create(name, email, password) {
+  async create({ name, email, password }) {
     const database = await sqliteConnection()
 
     const userId = await database.run(
